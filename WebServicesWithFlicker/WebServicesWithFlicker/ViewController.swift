9bfeb4e706f1e07e51d00ecfc48063931a22251f
Abhiladdy <abhilashiosdeveloper@gmail.com>
2018-11-16 16:19:43 -0500

    Creation of new project for webservices

---
+++ WebServicesWithFlicker/WebServicesWithFlicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebServicesWithFlicker
+//
+//  Created by Abhilash Reddy Jain on 11/16/18.
+//  Copyright © 2018 Abhilash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
